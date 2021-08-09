c0b0e2d0d3c06d498e2d4c56dc3d5ff3d2129e52
Hekuran <hekurangashi@pm.me>
2021-08-09 16:44:11 +0200

    trying out shell site

---
+++ other/shell.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+ bold=`echo -en "\e[1m"`
+ underline=`echo -en "\e[4m"`
+ dim=`echo -en "\e[2m"`
+ strickthrough=`echo -en "\e[9m"`
+ blink=`echo -en "\e[5m"`
+ reverse=`echo -en "\e[7m"`
+ hidden=`echo -en "\e[8m"`
+ normal=`echo -en "\e[0m"`
+ black=`echo -en "\e[30m"`
+ red=`echo -en "\e[31m"`
+ green=`echo -en "\e[32m"`
+ orange=`echo -en "\e[33m"`
+ blue=`echo -en "\e[34m"`
+ purple=`echo -en "\e[35m"`
+ aqua=`echo -en "\e[36m"`
+ gray=`echo -en "\e[37m"`
+ darkgray=`echo -en "\e[90m"`
+ lightred=`echo -en "\e[91m"`
+ lightgreen=`echo -en "\e[92m"`
+ lightyellow=`echo -en "\e[93m"`
+ lightblue=`echo -en "\e[94m"`
+ lightpurple=`echo -en "\e[95m"`
+ lightaqua=`echo -en "\e[96m"`
+ white=`echo -en "\e[97m"`
+ default=`echo -en "\e[39m"`
+ BLACK=`echo -en "\e[40m"`
+ RED=`echo -en "\e[41m"`
+ GREEN=`echo -en "\e[42m"`
+ ORANGE=`echo -en "\e[43m"`
+ BLUE=`echo -en "\e[44m"`
+ PURPLE=`echo -en "\e[45m"`
+ AQUA=`echo -en "\e[46m"`
+ GRAY=`echo -en "\e[47m"`
+ DARKGRAY=`echo -en "\e[100m"`
+ LIGHTRED=`echo -en "\e[101m"`
+ LIGHTGREEN=`echo -en "\e[102m"`
+ LIGHTYELLOW=`echo -en "\e[103m"`
+ LIGHTBLUE=`echo -en "\e[104m"`
+ LIGHTPURPLE=`echo -en "\e[105m"`
+ LIGHTAQUA=`echo -en "\e[106m"`
+ WHITE=`echo -en "\e[107m"`
+ DEFAULT=`echo -en "\e[49m"`
+
+
+echo "
+${blink}${bold}${lightaqua}${BLACK}
+┬ ┬┌─┐┬┌╴┬ ┬┬─┐┌─┐┌┐┬  ┌──┐┌─┐┌──┐┬ ┬┬
+├─┤├┤ ├┴┐│ │├┬┘├─┤│││  │ ┌┐├─┤└──┐├─┤│
+┴ ┴└─┘┴ └┴─┘┴└─┴ ┴┴└┘  └──┘┴ ┴└──┘┴ ┴┴
+${normal}
+
+${blink}${bold}${orange}Software:                      Websites:                     Blogs:                               RSS:${normal}
+${aqua}https://gitlab.com/Hekuran/    https://hekuran.gitlab.io/    https://hekuran.ga/blogindex.html    https://hekuran.ga/rss.html
+https://github.com/narukeh/    https://narukeh.github.io/    https://hekuran.ga/blog.html${normal}        └┬──────────────────────────┘
+                                                           ~┴────────────────────────────┘        │
+${blink}${bold}${white}Tungjatjeta!${normal}                                              ╱ ~─────────────────────────────────────┘
+                                                         ╱ ╱          ${blink}${bold}${orange}_____${normal}
+Im mosly active on Github.                              ╱ ╱         ${blink}${bold}${orange}\-     -/${normal}
+                                                       ╱ ╱       ${blink}${bold}${orange}\_/         \\${normal}
+You can read my blogs here. >─────────────────────────~ ╱        ${blink}${bold}${orange}|        O O |${normal}
+                                                       ╱         ${blink}${bold}${orange}|   <   )  3 )${normal}
+Dont have to visit my page just to read my            ╱          ${blink}${bold}${orange}/ \         /${normal}
+blogs.Simply subscribe to my RSS Feed with >─────────~              ${blink}${bold}${orange}/-_____-\\${normal}
+any RSS reader you want.So any time i post
+sth you will get an update.
+"
+
+# /bin/echo "
+
+#      _____      a
+#    \-     -/    a
+# \_/         \   a
+# |        O O |  a
+# |   <   )  3 )  a
+# / \         /   a
+#    /-_____-\    a
+
+# "
